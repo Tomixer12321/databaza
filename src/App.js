df14b4680af4474f5143688291dfd34bf3fd4b4c
Tomixer <tomix.klokar@gmail.com>
2023-11-20 20:17:06 +0100

    vypisovanie dat do stranky

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import { projectFirestore } from "./firebase/config"
+import { useState, useEffect } from "react"
 
 const App = () => {
-  return (
-    <div>ahoj</div>
-  )
+  const [data, setData] = useState([])
+  const [error, setError] = useState(false)
+  
+  useEffect( () => {
+    projectFirestore.collection("movies").get().then((snapshot)=>{
+
+      if(snapshot.empty){
+        setError("zadne filmy k vypisani")
+      }else{
+        let result=[]
+        snapshot.docs.forEach((oneMovie)=>{
+          result.push({id: oneMovie.id, ...oneMovie.data()})
+        })
+        setData(result)
+      }
+    } ).catch((err)=>{
+      setError(err.message)
+    })
+  }, [])
+
+  return <div>
+    {error && <p>{error}</p>}
+    {data.map((oneMovie)=>{
+
+      const {id,title,minage,time}=oneMovie
+
+      return <div key={id}>
+        <p>{title}, {time}min, {minage}+</p>
+      </div>
+    })}
+  </div>
 }
 
 export default App
